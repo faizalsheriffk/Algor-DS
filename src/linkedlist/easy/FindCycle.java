c78f6d346866b41035a2a3fb4f4ae4b443c59142
FaizalKSheriff <faizalsheriff@FaizalKSheriffs-MacBook-Pro.local>
2022-06-09 23:27:08 -0700

    linked list  easy and medium problems are committed

---
+++ src/linkedlist/easy/FindCycle.java
@@ -0,0 +1,33 @@
+package linkedlist.easy;
+
+public class FindCycle {
+    /**
+     * Using floyd cycle finding algorith which is nothing but
+     * two pointer algorithm, slow and fast.
+     * @param head
+     * @return
+     */
+    public boolean hasCycle(ListNode head) {
+
+        //following two pointer approach
+
+        if(head == null){
+            return;
+        }
+
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while(fast!=slow){
+
+            if(fast == null || fast.next == null){
+                false;
+            }
+
+
+            slow = head.next;
+            fast = head.next.next;
+        }
+        return true;
+    }
+}
